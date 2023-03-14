61f7dca322ce585323ce7f9e255be06b0718b84f
[zharri14] <[zharri14@uncc.edu]>
2023-03-14 14:20:42 -0400

    very simple signup

---
+++ ChromeExtensionProject/scripts/signup.js
@@ -0,0 +1,23 @@
+'use strict'
+
+document.querySelector('#signUpButton').addEventListener('click', function () {
+    let email = document.querySelector('#email').value;
+    createUser(email);
+    
+    window.location.pathname("/views/notes.html");
+});
+
+function createUser(email){
+    let firstName = document.querySelector('#firstName').value;
+    let lastName = document.querySelector('#lastName').value;
+    let password = document.querySelector('#password').value;
+
+    
+
+    let users = {};
+    let user = {email: email, password: password, firstName: firstName, lastName: lastName};
+    users[email] = JSON.stringify(user);
+    chrome.storage.sync.set(users, function(){
+        console.log(users);
+    });
+}
