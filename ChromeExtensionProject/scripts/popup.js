1a38d11a36953ed23f9c30fba09ed98a6a457b76
enealis <enealis@uncc.edu>
2023-03-13 21:38:08 -0400

    note save base funcanality working

--- ChromeExtensionProject/scripts/popup.js
+++ ChromeExtensionProject/scripts/popup.js
@@ -19,90 +19,57 @@ var notesAsObjects = [];
 var keys = [];
 var notesDiv = document.querySelector('#notes');
 var noteInput = document.querySelector('#noteInput');
-var ids = [];
-chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs=> {
+var ids = new Array();
+chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
     let url = tabs[0].url;
-   // alert(url);
-    chrome.storage.sync.get([url], function(items) {
-        keys = JSON.parse(items[url]);
-        console.log(keys);
-        alert(keys);
-        let notes = "";
-        // if(!notesAsObjects) {
-        //     notes = "";
-        // } else {
-        //     notesAsObjects.forEach(note => 
-        //         notes += note.note
-        //     );
-        // }
-        for (let i = 0; i < keys.length; i++) {
-            console.log(keys[i]);
-            chrome.storage.sync.get([String.toString(keys[i])], function(items) {
-                console.log(items[String.toString(keys[i])]);
-                let temp = items[String.toString(keys[i])];
-                notesAsObjects.push(temp);
-                console.log(temp);
-                alert(temp);
-        });
+    // alert(url);
+    chrome.storage.sync.get([url], function (items) {
 
-        }
-        notesAsObjects.forEach(note => {
-            notes += note.note;
-        });
-       // keys.forEach(key =>
-           // chrome.storage.sync.get(keys, function(items) {
-                // chrome.storage.sync.get([key], function(items) {
-                // notesAsObjects.push(items[key]);
-                // notes += items[key].note;
-                // alert(items[key]);
-            //     notesAsObjects = items[keys];
-            //     alert(notesAsObjects);
-            //     notesAsObjects.forEach(note=> 
+        if (items[url]) {
+            keys = JSON.parse(items[url]);
+            console.log(keys);
+            let notes = "";
+            // if(!notesAsObjects) {
+            //     notes = "";
+            // } else {
+            //     notesAsObjects.forEach(note => 
             //         notes += note.note
-            //         );
-            // });
-          //  }));
-           
-        notesDiv.append(notes);
+            //     );
+            // }
+            chrome.storage.sync.get(keys, function (items) {
+                console.log(items);
+                keys.forEach(key => {
+                    let noteObj = JSON.parse(items[key]);
+                    notesAsObjects.push(noteObj);
+                })
+                notesAsObjects.forEach(note => {
+                    notes += note.note;
+                });
+                notesDiv.append(notes);
+            });
+        }
 
     });
 
 });
 // on add note
-document.querySelector('#addNote').addEventListener('click', function(e) {
+document.querySelector('#addNote').addEventListener('click', function (e) {
     notesDiv.append(noteInput.value);
-
-    let newNote = {note: noteInput.value};
-
-    //notesAsObjects.push(newNote);
-    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs=> {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, async tabs => {
         let url = tabs[0].url;
-        let currentId = Date.now();
-        chrome.storage.sync.get([url], function(items) {
-            if(items[url]) {
+        chrome.storage.sync.get([url], function (items) {
+            console.log(items[url]);
+            if (typeof items[url] === "undefined") {
+                ids = [];
+                setNote(url);
+            } else {
                 ids = JSON.parse(items[url]);
+                chrome.storage.sync.remove([url], function () {
+                    //removes current list of ids so that same url key can be used agin
+                    console.log("old ids" + ids);
+                    setNote(url);
+                });
             }
-        });
-        ids.push(currentId);
-        let currentNote = JSON.stringify(newNote);
-        let currentIds = JSON.stringify(ids);
-        let json = {};
-        let json2= {};
-        json[currentId] = currentNote;
-        json2[url] = currentIds;
-        // if(!json[url]) {
-        //     json[url] = noteInput.value;
-        // } else {
-        //     json[url] += noteInput.value;
-        //}
-        chrome.storage.sync.set(json, function() {
-            console.log(newNote);
-            //stores notes in persistence storage based on id
-        });
-        chrome.storage.sync.set(json2, function() {
-            console.log(url);
-            console.log(ids);
-           //stores a list or ids with key url 
         });
 
         // chrome.storage.sync.set({url: ids});
@@ -110,11 +77,31 @@ document.querySelector('#addNote').addEventListener('click', function(e) {
         // chrome.storage.sync.set({currentId: newNote});
 
     });
-    noteInput.textContent = "";
 });
 
+function setNote(url) {
+    let currentId = Date.now();
+    ids.push(currentId.toString());
+    let newNote = { note: noteInput.value };
+    let currentNote = JSON.stringify(newNote);
+    let currentIds = JSON.stringify(ids);
+    let json = {};
+    let json2 = {};
+    json[currentId] = currentNote;
+    json2[url] = currentIds;
+    console.log(json);
+    console.log(json2);
+    chrome.storage.sync.set(json, function () {
+        //stores notes in persistence storage based on id
+        chrome.storage.sync.set(json2, function () {
+            //stores a list or ids with key url 
+            noteInput.textContent = "";
+        });
+    });
+}
 
-document.querySelector('#clearPage').addEventListener('click', function() {
+
+document.querySelector('#clearPage').addEventListener('click', function () {
     // chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs=> {
     //     let url = tabs[0].url;
     //     chrome.storage.sync.remove([url], function() {
