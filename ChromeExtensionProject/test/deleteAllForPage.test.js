fd4de0519f3ec5a117dfac1794153d9f261da577
ighall96 <123019909+IsaacGHall@users.noreply.github.com>
2023-04-11 23:47:08 -0400

    working on JSON format in test file
    UD4155-25
    As a programmer I want to be able to test our notes are being stored properly.
    
    Co-Authored-By: A-bmw <108549880+A-bmw@users.noreply.github.com>

--- ChromeExtensionProject/test/deleteAllForPage.test.js
+++ ChromeExtensionProject/test/deleteAllForPage.test.js
@@ -14,21 +14,31 @@ global.document = window.document;
 
 function deleteAllForPage() {
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, async tabs => {
+        console.log('tabs:', tabs);
         let url = tabs[0].url;
+        console.log('url:', url);
         chrome.storage.sync.get([url], function (items) {
+            console.log('items:', items);
+            console.log("items[url]:",items[url]);
             if (typeof items[url] === "undefined") {
                 //Empty list for URL
             } else {
-                let idsRemove = JSON.parse(items[url]);
-                chrome.storage.sync.remove(idsRemove, function () {
-                    chrome.storage.sync.remove([url]);
-                    //Removes notes from URL
-                    notesDiv.replaceChildren();
-                });
+                try {
+                    let idsRemove = JSON.parse(items[url]);
+                    chrome.storage.sync.remove(idsRemove, function () {
+                        console.log("Removed: ", idsRemove);
+                        console.log("chrome storage remove args (should not be empty):",chrome.storage.sync.remove.args);
+                        chrome.storage.sync.remove([url]);
+                        
+                        //Removes notes from URL 
+                        notesDiv.replaceChildren();
+                    });
+                } catch (e) {
+                    console.error('Parsing error:', e);
+                }
             }
-        });
-
-
+        
+    });
     });
 }
 
@@ -46,12 +56,12 @@ describe('deleteAllForPage', function() {
     const url = "https://google.com";
     const notesDiv = document.createElement('div');
     chrome.tabs.query.callsArgWith(1,[{url}]);
-    chrome.storage.sync.get.withArgs([url]).yields({});
+    chrome.storage.sync.get.withArgs([url]).yields({ [url] : 'value'});
 
     deleteAllForPage();
     //pass (for now)
-    console.log(chrome.storage.sync.remove.args);
-    expect(chrome.storage.sync.remove.calledWith([url])).to.be.false;
+    console.log("chrome storage remove args (should not be empty):",chrome.storage.sync.remove.args);
+    expect(chrome.storage.sync.remove.calledWith([url])).to.be.true;
 
     expect(notesDiv.children).to.be.empty;
 
